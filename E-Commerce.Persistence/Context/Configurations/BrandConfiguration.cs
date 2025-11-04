054152e8e4857d2eab6306d2416725f62c209bb0
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-11-04 16:47:10 +0200

    Session Demo

---
+++ E-Commerce.Persistence/Context/Configurations/BrandConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace E_Commerce.Persistence.Context.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<ProductBrand>
+    {
+        public void Configure(EntityTypeBuilder<ProductBrand> builder)
+        {
+            builder.Property(p => p.Name)
+                .HasColumnType("VarChar")
+                .HasMaxLength(256);
+        }
+    }
+}
---
+++ E-Commerce.Persistence/Context/StoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerce.Domain.Entities.Products;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+
+namespace E_Commerce.Persistence.Context
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
