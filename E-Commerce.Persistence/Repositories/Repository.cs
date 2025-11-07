84e5c0fe934fca2d9f0ee2d808d62300d36a668c
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-11-07 14:37:13 +0200

    Part03 Generic Repository

---
+++ E-Commerce.Persistence/Repositories/Repository.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities;
+using E_Commerce.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Persistence.Repositories
+{
+    public class Repository<TEntity, TKey>(StoreDbContext context) : IRepository<TEntity, TKey> where TEntity : Entity<TKey>
+    {
+        public void Add(TEntity entity) => context.Set<TEntity>().Add(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await context.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity>? GetByIdAsync(TKey id) => await context.Set<TEntity>().FindAsync(id);
+
+        public void Remove(TEntity entity) => context.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity) => context.Set<TEntity>().Update(entity);
+    }
+}
