cb97dd57fd3042efa600b6627489df9ea1704347
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-11-17 19:11:33 +0200

    Part03 Specification Evaluator

---
+++ E-Commerce.Persistence/Repositories/SpecificationEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce.Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Repositories
+{
+    public static class SpecificationEvaluator 
+    {
+        public static IQueryable<TEntity> ApplySpecification<TEntity>(this IQueryable<TEntity> inputQuery, 
+                                           ISpecification<TEntity> specification) where TEntity : class
+        {
+            IQueryable<TEntity> query = inputQuery;
+
+            if(specification.Criteria is not null)
+            {
+                query = query.Where(specification.Criteria);
+            }
+            query = specification.Includes.Aggregate(query, (query, expression) => query.Include(expression));
+            return query;
+        }
+    }
+}
---
+++ E-Commerce.Service/Specifications/BaseSpecification.cs
@@ -0,0 +1,25 @@
+﻿using E_Commerce.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Service.Specifications
+{
+    public abstract class BaseSpecification<TEntity> : ISpecification<TEntity> where TEntity : class
+    {
+        protected BaseSpecification(Expression<Func<TEntity, bool>>? criteria)
+        {
+            Criteria = criteria;
+        }
+        public Expression<Func<TEntity, bool>> Criteria {  get; private set; }
+
+        public ICollection<Expression<Func<TEntity, object>>> Includes {  get; private set; }
+        protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
