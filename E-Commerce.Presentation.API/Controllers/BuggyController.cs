2090e8a17a0301e4244b910744b54e43cb618980
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-11-30 08:33:18 +0200

    Part01 Error Types at API

---
+++ E-Commerce.Presentation.API/Controllers/BuggyController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Presentation.API.Controllers
+{
+    public class BuggyController : APIBaseController
+    {
+        [HttpGet("notfound")]
+        public IActionResult GetNotFoundResponse()
+        {
+            return NotFound();
+        }
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequestResponse()
+        {
+            return BadRequest();
+        }
+        [HttpGet("badrequest/{id}")]
+        public IActionResult GetValidationErrorResponse(int id)
+        {
+            return BadRequest();
+        }
+        [HttpGet("servererror")]
+        public IActionResult GetServerErrorResponse()
+        {
+            throw new Exception();
+            return BadRequest();
+        }
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnAuthorizedResponse()
+        {
+            return Unauthorized();
+        }
+    }
+}
