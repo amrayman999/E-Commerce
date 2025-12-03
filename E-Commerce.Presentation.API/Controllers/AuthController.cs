8867c4c59a2025584ca6e48dc1697ece03c436ff
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-12-03 18:28:55 +0200

    Part04 Security Module - Auth Controller[Login-Register]

---
+++ E-Commerce.Presentation.API/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using E_Commerce.Service.Abstraction;
+using E_Commerce.Shared.Dtos.Auth;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Presentation.API.Controllers
+{
+    public class AuthController(IServiceManager _serviceManager) : APIBaseController
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            var result = await _serviceManager.AuthService.LoginAsync(request);
+            return Ok(result);
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var result = await _serviceManager.AuthService.RegisterAsync(request);
+            return Ok(result);
+        }
+    }
+}
