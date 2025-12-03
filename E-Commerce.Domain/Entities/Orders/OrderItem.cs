a0563ad97409f2aac7c087932fa10bf56fe913af
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-12-03 20:51:13 +0200

    Part02 Order Modules - Models

---
+++ E-Commerce.Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities.Orders
+{
+    public class OrderItem : Entity<int>
+    {
+        public OrderItem() { }
+        public OrderItem(ProductInOrderItem product, decimal price, int quantity)
+        {
+            Product = product;
+            Price = price;
+            Quantity = quantity;
+        }
+        public ProductInOrderItem Product {  get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
