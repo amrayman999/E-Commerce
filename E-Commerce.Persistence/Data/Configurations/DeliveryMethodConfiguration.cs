55f3e2b85888804e7c79784878395a56740aa0d8
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-12-03 21:18:28 +0200

    Part03 Order Modules - Configurations

---
+++ E-Commerce.Persistence/Data/Configurations/DeliveryMethodConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Domain.Entities.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace E_Commerce.Persistence.Data.Configurations
+{
+    public class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(D => D.ShortName).HasColumnType("varchar").HasMaxLength(128);
+            builder.Property(D => D.Description).HasColumnType("varchar").HasMaxLength(256);
+            builder.Property(D => D.DeliveryTime).HasColumnType("varchar").HasMaxLength(128);
+            builder.Property(D => D.Price).HasColumnType("decimal(18,2)");
+
+
+        }
+    }
+}
